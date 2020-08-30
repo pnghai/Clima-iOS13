2b34f86cb25ef2f176a972b1f0215977c1a18abc
hai <pnghai@gmail.com>
2020-08-30 10:25:05 +0700

    add mvc logics

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTxtFld: UITextField!
     
+    var weatherManager = WeatherManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        weatherManager.delegate = self
+        searchTxtFld.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: Any) {
+        searchTxtFld.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTxtFld.endEditing(true)
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTxtFld.text != "" {
+            return true
+        }
+        else {
+            searchTxtFld.placeholder = "Type something"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTxtFld.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTxtFld.text = ""
+    }
+    func didUpdateWeather(weather: WeatherModel){
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
 }
 
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,57 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Hai Phan Nguyen on 28/08/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+struct WeatherManager {
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=061294efea2deff9dcf76a921f954f47&units=metric"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherUrl)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    func performRequest(urlString: String){
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let weather = self.parseJSON(weatherData: safeData) {
+                        self.delegate?.didUpdateWeather(weather: weather)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    func parseJSON(weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+}
